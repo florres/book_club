eca032996275a0ffaaba88a281ab54430f7cb62d
Florian Krämer <me@florres.de>
2012-06-30 23:47:24 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookClub::Application.config.secret_token = 'b8a6e6ec3e5142bc3d3af679db9e428cd8e2388e5bb4aded536aead2c1a3d182a29ca07e7c5eca374ffcea7589deaf2d5fb6c78e44defd30816f888a512b2ba6'
