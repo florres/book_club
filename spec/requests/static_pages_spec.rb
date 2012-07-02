49e0ba4b0e04aa40c112de66fb7719795b87ac28
Florian Krämer <me@florres.de>
2012-07-02 17:23:57 +0200

    add full title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,15 @@ describe "StaticPages" do
     before { visit '/static_pages/home' }
     
     it "should have the content 'Book Club'" do
-      page.should have_content('Book Club')
+      page.should have_selector('h1', :text => 'Book Club')
+    end
+
+    it "should have base title" do
+      page.should have_selector("title", :text => "Book Club")
+    end
+
+    it "should not have a custom page title" do
+      page.should_not have_selector('title', :text => "| Home")
     end
 
     describe 'User not logged in' do
@@ -13,6 +21,30 @@ describe "StaticPages" do
       it "should have the content 'Sign Up Now'" do
         page.should have_content('Sign Up Now')
       end
+    end
+  end
+
+  describe 'Help page' do
+    before { visit '/static_pages/help' }
+
+    it "should have page title ' | Help'" do
+      page.should have_selector('title', :text => " | Help")
+    end
+  end
+
+  describe 'About page' do
+    before { visit '/static_pages/about' }
+
+    it "should have page title ' | About'" do
+      page.should have_selector('title', :text => " | About")
+    end
+  end
+
+  describe 'Contact page' do
+    before { visit '/static_pages/contact' }
+  
+    it "should have page title ' | Contact'" do
+      page.should have_selector("title", :text => " | Contact")
     end
   end
 end
